c46c5f4ddd941c6100750c76ec6b3a1c6f6792bc
Luis A <luisrlvarezs@gmail.com>
2026-05-27 00:23:07 -0400

    fix(swagger): resolver error de referencia de __dirname en el entorno de ES modules

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,5 +1,9 @@
 import swaggerJSDoc from 'swagger-jsdoc';
+import { fileURLToPath } from 'url';
 import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const swaggerOptions: swaggerJSDoc.Options = {
   definition: {
